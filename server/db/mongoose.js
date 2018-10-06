456c828b99fdb61dab1a1e6f8aa86206645ca7a2
hohetro <everyday.paper.31@gmail.com>
2018-10-06 14:48:36 +0900

    Modify Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
-const {MONGODB_URI} = require('./../env.js');
+// const {MONGODB_URI} = require('./../env.js');
+var MONGODB_URI = 'mongodb://hohetro:uGw1vMp0ka7qm@ds125073.mlab.com:25073/node_todo';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
