66ff89594fb5ac12c769911ac6202039bcc43be5
hohetro <everyday.paper.31@gmail.com>
2018-10-06 13:49:17 +0900

    ADD mongoose-queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5bb361afa1eb2c06cc053599';
+
+if(!ObjectID.isValid(id)){
+  console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+  console.log('Todo By Id', todo);
+}).catch((e) => {
+  console.log(e);
+});
