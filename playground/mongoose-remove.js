5b0da00dc5d04b806e4ae93739a3919c0c8b3f29
hohetro <everyday.paper.31@gmail.com>
2018-10-07 16:44:56 +0900

    ADD Test delete

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,15 +4,7 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-var id = '5bb969e016cafed873d5b4ae';
-
 console.log(id);
-
-// Todo.remove({}).then((result) => {
-//   console.log(result);
-// });
-
-// Todo.findOneAndRemove({}).then();
 
 Todo.findByIdAndRemove(id).then((todo) => {
   console.log(todo);
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 
 // const {MONGODB_URI} = require('./../env.js');
-var MONGODB_URI = 'mongodb://hohetro:uGw1vMp0ka7qm@ds125073.mlab.com:25073/node_todo';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
