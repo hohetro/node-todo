308eead5225b4ccbac7f4bff556e78057ea84646
hohetro <everyday.paper.31@gmail.com>
2018-10-08 18:12:15 +0900

    ADD GET /users/me & auth

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -109,6 +110,21 @@ app.post('/users', (req, res) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
+  });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if(!user){
+      return Promise.reject();
+    }
+    res.send(user);
+  }).catch((e) => {
+    res.status(401).send();
   });
 });
 
